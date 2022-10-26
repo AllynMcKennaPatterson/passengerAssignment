ae757b96fb11b8dfe7510d920647a9869d120db7
AllynMcKennaPatterson <G00392765@atu.ie>
2022-10-26 13:46:27 +0100

    Added constructor

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,4 +1,13 @@
 package ie.atu;
 
 public class Passenger {
+    private String title;
+    private String name;
+    private String id;
+    private String phone;
+    private int age;
+
+    public Passenger(String[] title, String[] name, String[] id, String[] phone, int age){
+
+    }
 }
