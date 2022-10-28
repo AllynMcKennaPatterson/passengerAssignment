d47aa1b492172244b3bf7c8552d897de9d8cd127
AllynMcKennaPatterson <G00392765@atu.ie>
2022-10-28 12:25:04 +0100

    Created testTitleFail()

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -1,4 +1,33 @@
 package ie.atu;
 
 public class Passenger {
+    private String title;
+    private String name;
+    private String id;
+    private String phone;
+    private int age;
+
+    public Passenger(String title, String name, String id, String phone, int age){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/test/java/ie/atu/PassengerTest.java
+++ src/test/java/ie/atu/PassengerTest.java
@@ -2,16 +2,30 @@ package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
+    Passenger newPassenger;
 
     @BeforeEach
     void setUp() {
     }
 
+    @Test
+    void testTitleFail(){
+        newPassenger = new Passenger("Mr", "Allyn", "1287gf8237g", "0976284", 21);
+        assertTrue(newPassenger.getTitle().equals("Mr") || newPassenger.getTitle().equals("Ms"));
+        assertTrue(newPassenger.getName().length() >= 3);
+        assertTrue(newPassenger.getId().length() >= 10);
+        assertTrue(newPassenger.getPhone().length() >= 7);
+        assertTrue(newPassenger.getAge()> 16);
+    }
+
     @AfterEach
     void tearDown() {
+        //newPassenger = new Passenger();
     }
 }
